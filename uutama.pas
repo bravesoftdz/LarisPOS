b54493cd3a1f8da618b5fe0c5368b95ce59a9937
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-01-21 01:03:43 +0700

    ubah nama form utama dan awal ui.

--- uutama.pas
+++ uutama.pas
@@ -5,22 +5,52 @@ unit uUtama;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus,
+  ComCtrls, ExtCtrls, ActnList;
 
 type
-  TForm1 = class(TForm)
+
+  { TFrmUtama }
+
+  TFrmUtama = class(TForm)
+    ActList: TActionList;
+    MainMenu: TMainMenu;
+    Page: TPageControl;
   private
     { private declarations }
   public
+    procedure TambahFormTab(Form: TForm);
+    procedure HapusFormTab(Sender: TObject; var CloseAction: TCloseAction);
     { public declarations }
   end;
 
 var
-  Form1: TForm1;
+  FrmUtama: TFrmUtama;
 
 implementation
 
 {$R *.lfm}
+
+{ TFrmUtama }
+
+procedure TFrmUtama.TambahFormTab(Form: TForm);
+var
+  Tab: TTabSheet;
+begin
+  Tab:= Page.AddTabSheet;
+  Form.Parent:= Tab;
+  Form.OnClose:=@HapusFormTab;
+  Page.ActivePage:= Tab;
+  Form.Show;
+  Tab.Caption:= Form.Caption;
+end;
+
+procedure TFrmUtama.HapusFormTab(Sender: TObject;
+  var CloseAction: TCloseAction);
+begin
+  CloseAction:=caFree;
+  TTabSheet(TForm(Sender).Parent).Free;
+end;
 
 end.
 
