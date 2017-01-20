e0910f99d4bab5ea5292b326e671f9e8898a544c
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-01-20 22:35:08 +0700

    tambah dbutils

---
+++ utils/dbutils.pas
@@ -0,0 +1,45 @@
+unit dbutils;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  dSQLdbBroker, SysUtils, sqldblib, IniFiles, mysql56conn;
+
+function con: TdSQLdbConnector;
+
+implementation
+
+var
+  _con: TdSQLdbConnector = nil;
+
+function con: TdSQLdbConnector;
+var
+  LibLoad: TSQLDBLibraryLoader;
+  INI: TIniFile;
+begin
+  if not Assigned(_con) then
+  begin
+    INI:= TIniFile.Create(GetAppConfigFile(False));
+
+    _con := TdSQLdbConnector.Create(nil);
+    //_con.Logger.Active := True;
+    //_con.Logger.FileName := 'OUTPUT.LOG';
+    _con.User:= INI.ReadString('DB','UserName', 'test_user');
+    _con.Password:= INI.ReadString('DB','Password', '');
+    _con.Database:= INI.ReadString('DB','DatabaseName', 'test');
+    _con.Host:= INI.ReadString('DB','HostName', '127.0.0.1');
+
+    LibLoad:= TSQLDBLibraryLoader.Create(nil);
+    LibLoad.ConnectionType:= INI.ReadString('DB','ConnectionType', 'MySQL 5.6');
+    LibLoad.LibraryName:= INI.ReadString('DB', 'LibraryName', '/opt/lampp/lib/libmysqlclient.so.18.0.0');
+    LibLoad.LoadLibrary;
+  end;
+end;
+
+finalization
+  FreeAndNil(_con);
+
+end.
+
