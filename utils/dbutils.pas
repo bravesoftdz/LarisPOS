003e9f7606ce2a4a389b5d11057b2720b46f0326
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-01-21 22:41:25 +0700

    ubah dbutils agar bisa dijalankan unit test

--- utils/dbutils.pas
+++ utils/dbutils.pas
@@ -11,37 +11,30 @@ function con: TdSQLdbConnector;
 
 implementation
 
-var
-  _con: TdSQLdbConnector = nil;
-
 function con: TdSQLdbConnector;
 var
   LibLoad: TSQLDBLibraryLoader;
   INI: TIniFile;
+  _con: TdSQLdbConnector;
 begin
-  if not Assigned(_con) then
-  begin
-    INI:= TIniFile.Create(GetAppConfigFile(False));
+  INI:= TIniFile.Create(GetAppConfigFile(False));
 
-    _con := TdSQLdbConnector.Create(nil);
-    //_con.Logger.Active := True;
-    //_con.Logger.FileName := 'OUTPUT.LOG';
-    _con.User:= INI.ReadString('DB','UserName', 'larispos');
-    _con.Password:= INI.ReadString('DB','Password', 'larispos');
-    _con.Database:= INI.ReadString('DB','DatabaseName', 'larispos');
-    _con.Host:= INI.ReadString('DB','HostName', '127.0.0.1');
-    _con.ConnectorType:=INI.ReadString('DB','ConnectionType', 'MySQL 5.6');
+  _con := TdSQLdbConnector.Create(nil);
+  //_con.Logger.Active := True;
+  //_con.Logger.FileName := 'OUTPUT.LOG';
+  _con.User:= INI.ReadString('DB','UserName', 'larispos');
+  _con.Password:= INI.ReadString('DB','Password', 'larispos');
+  _con.Database:= INI.ReadString('DB','DatabaseName', 'larispos');
+  _con.Host:= INI.ReadString('DB','HostName', '127.0.0.1');
+  _con.ConnectorType:=INI.ReadString('DB','ConnectionType', 'MySQL 5.6');
 
-    LibLoad:= TSQLDBLibraryLoader.Create(nil);
-    LibLoad.ConnectionType:= _con.ConnectorType;
-    LibLoad.LibraryName:= INI.ReadString('DB', 'LibraryName', '/opt/lampp/lib/libmysqlclient.so.18.0.0');
-    LibLoad.LoadLibrary;
-    Result:=_con;
-  end;
+  LibLoad:= TSQLDBLibraryLoader.Create(nil);
+  LibLoad.ConnectionType:= _con.ConnectorType;
+  LibLoad.LibraryName:= INI.ReadString('DB', 'LibraryName', '/opt/lampp/lib/libmysqlclient.so.18.0.0');
+  LibLoad.LoadLibrary;
+
+  Result:=_con;
 end;
-
-finalization
-  FreeAndNil(_con);
 
 end.
 
