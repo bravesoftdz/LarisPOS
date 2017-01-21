8a9acabff4ef0acd90c0b5cfd1c36a23a41e372a
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-01-22 01:26:04 +0700

    model kategori

---
+++ models/kategori.pas
@@ -0,0 +1,22 @@
+unit kategori;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  dOpf, dSQLdbBroker;
+type
+  TKategori = class
+  private
+    FId: string;
+    FKategori: string;
+  published
+    property Id: string read FId write FId;
+    property Kategori: string read FKategori write FKategori;
+  end;
+
+implementation
+
+end.
+
